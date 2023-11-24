5e5dc14a56e0b0a9e76b18665727e47390e0d09c
Vini <vsgh-gouveia@hotmail.com>
2023-11-23 23:48:38 -0300

    Estrutura inicial da header criada

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,46 @@
+import SubHeader from "./SubHeader"
 
 const Header = () => {
   return (
     <header>
+      <SubHeader
+        address="Lago Sul, Brasília - DF"
+        opens="08:00"
+        closes="18:00"
+        phone="(61) 3395-8080"
+        phone2="(61) 3359-8181"
+        whatsapp="5561999999999"
+        instagram="smartseguranca"
+        facebook="smartseguranca"
+        email="contato@smartseguranca.com.br"
+      />
+      <nav>
+        <div>
+          <a href="/" className="w-fit flex items-center gap-2">
+            <img src="/images/logo.png" alt="Smart Segurança" className="w-10"/>
+            <p className="text-xl">
+              Smart Segurança
+            </p>
+          </a>
+        </div>
+        <ul>
+          <li>
+            <a href="/">Início</a>
+          </li>
+          <li>
+            <a href="/sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="/contato">Planos</a>
+          </li>
+          <li>
+            <a href="/contato">Atendimento</a>
+          </li>
+          <li>
+            <a href="/contato">Contrate</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
