71c6186e85f20559350ae5041e25da5758886b92
Vini <vsgh-gouveia@hotmail.com>
2023-11-24 01:24:02 -0300

    Cor de fundo ao body (#101010)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header"
 
 function App() {
+  // Mudar a cor de fundo do body
+  document.body.classList.add("bg-[#101010]", "text-neutral-100");
 
   return (
     <Header />
